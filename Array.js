fabd73ac605898f354fdd6dcb3c61f7733b2ea0e
rafi4863 <rafi2626r@gmail.com>
2025-10-01 14:06:33 +0600

    javascript learn

---
+++ Array.js
@@ -0,0 +1,30 @@
+
+// Array type system
+
+var numbers = [45, 32, 54, 20, 87, 48, 64, 50, 100, 200];
+
+console.log("Numbers is : " + numbers);
+
+
+
+
+var friendsName = ["Sumon", "Shanto", "Sohel", "Robin", "Rabbi", "Himel"];
+
+console.log("My friends name is : " + friendsName);
+
+
+
+// Array length check
+
+var friendsAge = [20, 21, 23, 25, 19, 18, 22];
+
+console.log( "My friendsAge length is : " + friendsAge.length);
+
+
+
+
+var friendsName = ["Sumon", "Shanto", "Sohel", "Robin", "Rabbi", "Himel"];
+
+console.log( "My friendsName length is : " + friendsName.length);
+
+
---
+++ dataType.js
@@ -0,0 +1,30 @@
+
+// string type
+
+var name = ("Rafi, Rabbi, Himel, Shihab");
+
+console.log(name);
+
+
+// number type 
+
+var eggPrice = (60);
+
+console.log(eggPrice);
+
+
+// boolean data type
+
+
+
+console.log(  5 > 10);
+
+console.log(  50 < 70);
+
+
+// boolean data type check
+
+
+console.log( typeof 5 > 10);
+
+console.log( typeof 50 < 70);
