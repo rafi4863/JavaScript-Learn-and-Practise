a63ac35b5fad2917ba3cdd72419e75e784cb5a1d
rafi4863 <rafi2626r@gmail.com>
2025-10-01 21:09:07 +0600

    push&pop

---
+++ pushAndPop.js
@@ -0,0 +1,34 @@
+
+// push some numbers in array
+
+var addNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+addNumbers.push(10, 10, 11, 12);
+
+console.log(addNumbers);
+
+
+// push some string in array
+
+var addNames = ["Emon", "Sabbir", "Hridoy", "Tamim", "Liton", "Shamim"];
+
+addNames.push("Rohim", "Rabbi", "Himel");
+
+console.log(addNames);
+
+
+
+// pop some number and string in Array
+
+
+var addNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+addNumbers.pop();
+addNumbers.pop();
+addNumbers.pop();
+
+console.log(addNumbers);
+
+
+
+
