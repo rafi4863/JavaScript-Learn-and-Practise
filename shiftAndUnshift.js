b27ab48de3c173580910ac0f248d16c81df2a0c7
rafi4863 <rafi2626r@gmail.com>
2025-10-01 22:37:05 +0600

    shift and unshift

---
+++ shiftAndUnshift.js
@@ -0,0 +1,55 @@
+
+// unshift method start
+
+var x = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+x.unshift(1);
+
+console.log(x);
+
+
+
+var y = ["Rabbi", "Himel", "Sabit", "Halim", "Roman", "Rohim"];
+
+y.unshift("Rafi");
+
+console.log(y);
+// Rafi name ta sobar samne add hoye jacce
+
+
+
+
+// shift method start
+
+var box_1 = [ 3, 5, 7, 9, 11, 13, 15];
+
+box_1.shift();
+
+console.log(box_1); 
+// first number 3 delete hoye jacce.
+
+
+
+// sort method code start
+
+var x = [5, 7, 3, 1, 2, 4, 9, 6, 8];
+
+x.sort();
+
+console.log(x);
+// sirial onosare sajabe
+
+
+
+// string sort method code start
+
+var names = ["Emon", "Faruq", "Habib", "Golam", "Abir", "Chayan", "Babul", "Jamil", "Imran", "Dipo", "Kamal"];
+
+names.sort();
+
+console.log(names);
+// A thake K porjonto sirial onojayi sob name sajiye diyece.
+
+
+
+
