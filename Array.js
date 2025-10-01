80e60cc044e387f6255dc38002b1c338ebb0161a
rafi4863 <rafi2626r@gmail.com>
2025-10-01 14:47:07 +0600

    Array

--- Array.js
+++ Array.js
@@ -4,6 +4,12 @@
 
 var numbers = [45, 32, 54, 20, 87, 48, 64, 50, 100, 200];
 
+console.log(numbers);
+
+
+
+var numbers = [45, 32, 54, 20, 87, 48, 64, 50, 100, 200];
+
 console.log(typeof numbers);
 
 
@@ -34,5 +40,35 @@ console.log( "My friendsAge length is : " + friendsAge.length);
 var friendsName = ["Sumon", "Shanto", "Sohel", "Robin", "Rabbi", "Himel"];
 
 console.log( "My friendsName length is : " + friendsName.length);
+
+
+// check Array indexof numbers
+
+var numbers = [45, 32, 54, 20, 87, 48, 64, 50, 100, 200];
+
+console.log("The indexof is : " + numbers.indexOf(87));
+
+
+// check string indexoff numbers
+
+var friendsName = ["Sumon", "Shanto", "Sohel", "Robin", "Rabbi", "Himel"];
+
+console.log( "The indexof number is : " + friendsName.indexOf("Robin"));
+
+
+
+// index Element check
+
+var friendsAge = [20, 21, 23, 25, 19, 18, 22];
+
+var indexElement = friendsAge[4];
+
+console.log(indexElement);
+
+
+
+
+
+
 
 
