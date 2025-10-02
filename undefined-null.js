3062a88aaba8008d08deed8ac0d386f11b2be1eb
rafi4863 <rafi2626r@gmail.com>
2025-10-02 15:54:50 +0600

    undefinded&null

---
+++ undefined-null.js
@@ -0,0 +1,19 @@
+
+var x;
+
+console.log(x);
+// output is : undefined
+
+var y;
+
+console.log(typeof y);
+// output is : undefined
+
+
+var x = null;
+
+console.log( typeof null);
+
+// output is : Object
+
+
