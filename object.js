462b6925fb3d526b4dbeb0ada9d643d7010a64d8
rafi4863 <rafi2626r@gmail.com>
2025-10-02 13:09:01 +0600

    object.js

---
+++ object.js
@@ -0,0 +1,16 @@
+
+// the rules of javascript object
+
+var rafi = {
+    fullname : "Md Al-Rafi",
+    age : 22,
+    address : "Naogoan, Rajshahi",
+    email : "mdrafi2626r@gmail.coom",
+    contact : "01787215170",
+    height : 5.5,
+    relationship : "married",
+    institute : "Bogura Polytechnic institute",
+    Roll : 734389
+};
+
+console.log(rafi.institute);
