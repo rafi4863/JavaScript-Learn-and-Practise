0a690972b3f102d66d5a0e08da469b85372b0828
rafi4863 <rafi2626r@gmail.com>
2025-10-03 11:58:11 +0600

    math

---
+++ mathmatical-oparation.js
@@ -0,0 +1,66 @@
+
+// javascript addition oparation
+
+const eggPrice = 60;
+
+const saltPrice = 40;
+
+ const total = eggPrice + saltPrice;
+
+ console.log( "My total cost : " + total + " taka.");
+
+
+// javascript substraction oparation
+
+
+ const myBudjet = 50000;
+
+ const pcPrice = 30000;
+
+ const monitorPrice = 10000;
+
+ const keyboardPrice = 1500;
+
+ const mousePrice = 1000;
+
+ const soundBoxPrice = 2000;
+
+
+ const remainingMoney = ( + myBudjet - pcPrice - monitorPrice - keyboardPrice - mousePrice - soundBoxPrice);
+
+ console.log( "Now my remaning Money is : " + remainingMoney + " taka.");
+
+
+
+//  javascript multiplication oparation
+
+
+let x = 3;
+
+let y = 3;
+
+let z = 3;
+
+const multiplication = (x * y * z);
+
+console.log(multiplication);
+
+
+// javascript divition oparation
+
+let A = 50;
+
+let B = 2;
+
+const divition = A / B;
+
+console.log(divition);
+
+
+
+
+
+ 
+
+
+ 
