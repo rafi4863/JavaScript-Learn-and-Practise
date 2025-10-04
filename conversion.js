ba304985ab2247efc239f06b8e2380e7c2a24156
rafi4863 <rafi2626r@gmail.com>
2025-10-04 18:35:58 +0600

    conversion.js

---
+++ conversion.js
@@ -0,0 +1,66 @@
+
+// convert string to number
+
+let number1 = "40";
+
+let number2 = 20;
+
+const convert = parseInt(number1);
+
+console.log(convert);
+
+
+// convert string to only intiger number
+
+
+let x = "88.454355";
+
+let y = 20;
+
+const resultt = parseInt(x);
+
+console.log(resultt);
+
+
+
+// convert float-string to number
+
+let score = "21.456478"
+
+const result = parseFloat(score);
+
+console.log(result);
+
+
+
+// one example of javascript conversation
+
+
+let score1 = 14.5453;
+
+let score2 = 50;
+
+let score3 = 10.5454;
+
+const total = parseInt(score1 + score2 + score3);
+
+console.log(total);
+
+// output : 75 ( only interger number print )
+
+
+
+// convert number to string
+
+
+let A = 22;
+
+let B = 50;
+
+const conv = A.toString();
+const conver = B.toString();
+
+console.log(conv);
+console.log(conver);
+
+
