40e8a2e3e5427ceca5d4f5a2e86f0af92e536fa8
rafi4863 <rafi2626r@gmail.com>
2025-10-04 20:35:11 +0600

    condition

--- condition.js
+++ condition.js
@@ -10,3 +10,35 @@ if (myBuget <= bargarPrice) {
     console.log("Amar kase bargar kinar moto taka nai");
     
 };
+
+
+
+// use if else condition for example
+
+
+let mark = 11;
+
+if ( mark >= 90){
+    console.log("GPA : Golden");
+}
+else if (mark >= 80) {
+    console.log("GPA : A+");
+}
+else if (mark >= 70) {
+    console.log("GPA : A");
+}
+else if (mark >= 60) {
+    console.log("GPA : A-");
+}
+else if (mark >= 50) {
+    console.log("GPA : B");
+}
+else if (mark >= 40) {
+    console.log("GPA : C"); 
+}
+else if (mark >= 33) {
+    console.log("Konorokom pass korece.");
+}
+else{
+    console.log("Se Exam a fail korece.");
+}
