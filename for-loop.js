5156c56733571b0168c1a89a0d01e7ca9bee9fb6
rafi4863 <rafi2626r@gmail.com>
2025-10-06 00:25:11 +0600

    for loop

--- for-loop.js
+++ for-loop.js
@@ -14,3 +14,18 @@ for( let score = 1; score <= 5; score++ ){
 for (let number = 1; number <= 20; number += 2){
     console.log(number);
 }
+
+
+
+// for loop ar maddhome jokon kono nirdisto data pabo tokon loop ke theme deoyar javaScript foor loop example
+
+console.log("for loop ar maddhome jokon kono nirdisto data pabo tokon loop ke theme deoyar javaScript foor loop example");
+
+
+for( let i = 0; i <= 10; i++){
+    console.log(i);
+    if ( i == 7) {
+        console.log("7 digit is detected.");
+        break;   
+    }
+}
