f8d4ea5bb06dc2ace8c841f19d4df76b9803e2ba
rafi4863 <rafi2626r@gmail.com>
2025-10-05 14:58:58 +0600

    while loop

---
+++ while-loop.js
@@ -0,0 +1,34 @@
+
+
+// example 1
+
+let score = 1;
+
+while (score <= 5) {
+    console.log("Hello JavaScript");
+    score++;
+}
+
+// example 2
+// while loop use kore array print kora
+
+
+let myArray = ["Rahim", "Karim", "Sabbir", "Minhaj"];
+
+let count = 0;
+
+while (count < myArray.length) {
+    console.log(myArray);
+    count++;
+}
+
+// example 3
+// while loop use kore 10 -1 porjonto number print kora
+
+
+let number = 10;
+
+while (number >= 1) {
+    console.log(number);
+    number--;
+}
