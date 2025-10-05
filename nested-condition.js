e71b35116e6daa16d861932affcb6584631663d8
rafi4863 <rafi2626r@gmail.com>
2025-10-05 14:41:35 +0600

    nested condition

---
+++ nested-condition.js
@@ -0,0 +1,15 @@
+
+let a = 10;
+let b = 20;
+let c = 30;
+
+if (a < b) {
+    if (b < c) {
+        console.log("C is big number");
+        
+    }
+}
+else{
+    console.log("something wrong");
+    
+}
