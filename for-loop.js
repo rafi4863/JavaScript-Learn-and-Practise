801aa41db831a4ba89b6e8f56139d8154d2821e3
rafi4863 <rafi2626r@gmail.com>
2025-10-06 00:15:47 +0600

    for loop

---
+++ for-loop.js
@@ -0,0 +1,16 @@
+
+// for loop example
+
+for( let score = 1; score <= 5; score++ ){
+    console.log("Hello JavaScript");
+}
+
+
+// example 2
+
+// for loop ar maddhome 1-20 number ar moddho bijor number golo print kora
+
+
+for (let number = 1; number <= 20; number += 2){
+    console.log(number);
+}
