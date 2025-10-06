0112598425d48d5fbdd0d93a3d8cd331236859ef
rafi4863 <rafi2626r@gmail.com>
2025-10-06 20:19:01 +0600

    function

---
+++ function.js
@@ -0,0 +1,68 @@
+
+// the structure of javaScript function
+
+function myfunction(params) {
+    
+}
+
+// function example
+
+
+function myfun(name, district){
+    
+    console.log("My name is " + name + " and i'm from " + district +".");
+    
+
+}
+
+myfun("Rafi", "Naogaon");
+
+myfun("Rabbi", "Jashore");
+
+myfun("Himel", "Magura");
+
+
+
+
+// next example
+// function use kore 2 ta number ar moddho jogful ber kora
+
+function addNumber (num1, num2) {
+    console.log(num1 + num2);
+    
+}
+addNumber(50, 50);
+
+
+// next example
+// return use kore 2 ta number ar moddho multiplication
+
+
+function multiplication(num1, num2) {
+    let multi =  num1 * num2;
+    return multi;
+}
+
+ const result =  multiplication(5, 5);
+
+ console.log(result);
+
+
+
+
+ // next example
+// return use kore 2 ta number ar moddho division
+
+
+function division(money) {
+    let eggPrice = 40;
+
+   let total =  money / eggPrice;
+   return total;
+}
+
+ const priceResult = division(500);
+
+ console.log(priceResult);
+ 
+ 
