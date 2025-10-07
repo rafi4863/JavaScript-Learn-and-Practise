66972171b02ede0b340b9ba7ad553ca59fe10def
rafi4863 <rafi2626r@gmail.com>
2025-10-07 17:19:40 +0600

    spread

---
+++ spread-oparator.js
@@ -0,0 +1,20 @@
+
+// spread oparator ar madddhome array ar moddho sobcheye coto number ber kora
+
+let array = [5, 2, 8, 5, 7, 6, 12];
+
+const result = Math.min(...array);
+
+console.log(result);
+
+
+
+
+
+// spread oparator ar madddhome array ar moddho sobcheye big number ber kora
+
+let array2 = [5, 2, 8, 5, 7, 6, 12];
+
+const result2 = Math.max(...array);
+
+console.log(result2);
