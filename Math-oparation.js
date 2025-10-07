2bf8bc1537a1d827926d69e5ca8992beaf44743d
rafi4863 <rafi2626r@gmail.com>
2025-10-07 20:32:52 +0600

    math

--- Math-oparation.js
+++ Math-oparation.js
@@ -20,3 +20,35 @@ console.log(result2);
 //ahane ceil dile 5.4 i mean jodi .ar pore kono number thake tahole ceil kore 6 baniye dibe... jodi 7.9 hoto taile output asto 8.
 
 
+
+// example 3
+
+let z = 4.3;
+
+const result3 = Math.floor(z);
+
+console.log(result3);
+// ahhane floor use korle jodi float number thake sei float numbers golo ke integer a convert korbe
+
+
+// example 4
+
+let a = 2.3;
+
+const result4 = Math.trunc(a);
+
+console.log(result4);
+// trucc use korle float ke integer kore dibe
+
+
+
+// example 5
+
+let B = -40;
+
+const result5 = Math.abs(B);
+console.log(result5);
+
+// abs use korle negetiv number ke positive kore dibe
+
+
