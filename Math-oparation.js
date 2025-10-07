3204135d269207e059802c15ce4b2c1295a3a816
rafi4863 <rafi2626r@gmail.com>
2025-10-07 17:52:17 +0600

    math

---
+++ Math-oparation.js
@@ -0,0 +1,20 @@
+
+// example 1
+
+let x = 5.10;
+
+const result =  Math.round(x);
+
+console.log(result);
+// akhane round dile 5.5 thake soro kore 5.9 porjonto kono number type hole output asbe 6,, and 5.4 thake 5.0 porjonto hole output asbe only 5.
+
+
+
+// example 2
+
+let y = 7.3;
+
+const result2 = Math.ceil(y);
+
+console.log(result2);
+//ahane ceil dile 5.4 i mean jodi .ar pore kono number thake tahole ceil kore 6 baniye dibe... jodi 7.9 hoto taile output asto 8.
