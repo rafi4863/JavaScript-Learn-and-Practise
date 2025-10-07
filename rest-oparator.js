0aa0b7227fe4c7163a879f3bc2e13ca40d9fa30c
rafi4863 <rafi2626r@gmail.com>
2025-10-07 11:18:20 +0600

    rest

---
+++ rest-oparator.js
@@ -0,0 +1,10 @@
+
+function restOparator (...numbers) {
+    return numbers;  
+}
+
+console.log(restOparator(10, 20, 30, 40, 50, 60));
+
+
+// rest oparator Symbol ( ... )
+// oporer example a function ar moddho only akta paramiter neoya hoyece..kinto user onekgolo paramiter value use korce. kinto user koto value pass korbe seta tw jani na. ajonno rest oparator use kora hoi. rest oprator use korle user jotoi value dik seta print korbe .  
